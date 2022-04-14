48d12e9f04f752253f3ebf9fcdde38cfa0953e38
nonnax <ironald@gmail.com>
2022-04-14 21:10:52 +0800

    refactor

--- config.ru
+++ config.ru
@@ -1,29 +1,31 @@
 #!/usr/bin/env bash
 # Id$ nonnax 2022-03-21 21:31:49 +0800
 require_relative 'lib/statics'
+require 'yaml'
 
-use Rack::Static, 
-  :urls => ["/images", "/js", "/css"],
-  :root => 'public'
 
-get('/') {
-  'index.html'
+get("/") {
+  res.write 'honey, im home!'
 }
 
-get('/home') {
-  'index.html'
+get("/home") {
+  file 'index.html'
 }
 
-get('/note') { |param|
+get("/note") { |param|
   param['name'] ||= 'note'
-  "#{param['name']}.html"
+  file "#{param['name']}.html"
 }
 
-get('/red') {
+get("/red") {
   halt [302, {'Location'=>'/note'}, []]
 }
+
 get '/text',  'note.html'
 
-p Map.map
+Map['/hq']='index.html'
+Map['/bahay']='kubo.html'
 
-run Map
+puts Map.map.to_yaml
+
+run Map.new
--- lib/statics.rb
+++ lib/statics.rb
@@ -1,24 +1,38 @@
 #!/usr/bin/env ruby
 # Id$ nonnax 2022-04-07 22:45:23 +0800
 D=Object.method(:define_method)
+F=File
 module Map
-  extend self
   routes={}
   D.(:map){ routes }
+  D.(:file) do |f| F.join('public', f).then{|f| res.write(F.read(f)) if F.exist?(f) } end
+  D.(:[]=){ |k, v| routes[k]=->(_){ file(v) }}
   D.(:req){ @req }
+  D.(:res){ @res }
   D.(:halt){ |r| throw :halt, r }
-  D.(:get){|u, f=nil, &b|
-    b ||= proc {f}
-    routes[u]=b
-  }
-  def self.call(env)
-    @req=Rack::Request.new(env)
-    catch(:halt) do
-      block=map.dup[env['PATH_INFO']] if env['REQUEST_METHOD']=='GET'
-      f=File.join('public', instance_exec(req.params, &block))
-      body = File.readlines(f, 'r').map if File.exist?(f)
-      return [200, {'Content-type' => 'text/html'}, body] if body
-      [404, {}, ['Not found']]
+  D.(:get){|u, f=nil, &b| b ||= ->(_){file(f)}; routes[u]=b  }
+  def self.new
+    @stack=Rack::Builder.new do   
+      use Rack::Static, :root => 'public', :urls => ["/images", "/js", "/css"] 
+      run App.new
+    end
+    @stack
+  end
+  class App
+    def call(env)
+      @req=Rack::Request.new(env)
+      @res=Rack::Response.new
+      res.headers['Content-type']='text/html; charset=utf-8'
+      catch(:halt) {
+        block=map.dup[env['PATH_INFO']] if env['REQUEST_METHOD']=='GET'
+        instance_exec(req.params, &block) #rescue nil
+        return res.finish unless res.body.empty?
+        [404, {}, ['Not found']]
+      }
+      rescue Exception=>e
+        [500, {}, [e.message]]
     end
   end
 end
+
+
